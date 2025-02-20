ab9c0b2840eaa5ce57bba26c427c10ecd3bc0b60
Isroilov997 <isroilovmuhammadsolih055@gmail.com>
2025-02-20 10:36:33 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,13 @@
+
+
+
+const a=prompt("string type malumot kiriting")
+const b=prompt("number type malumot kiriting")
+const c=prompt("boolean type malumot kiriting")
+
+confirm( "Kiriltilgan narsalar tog'rimi?" )
+
+alert( `Siz kiritgan ${ a } so'zi  ${ typeof a } type da
+Siz kiritgan ${ b }   ${ typeof b } type da
+Siz kiritgan ${ c } so'zi  ${ typeof c } type da`
+)
